5656321b09584d84b9d2326ff5c884f6760af8e4
allupro <aleksi.eskola100@gmail.com>
2022-03-20 17:45:44 +0200

    some changes

--- script.js
+++ script.js
@@ -1,62 +1,66 @@
 let current
-const cellStyles = 'cell placeholder:text-black bg-gray-200 border-black border h-8 w-20 '
-const Letters = ' ABCDEFGHIJKLMNOPQRSTU'
+const cellStyles = 'cell bg-gray-200 border-black border h-8 w-20 '
+const Letters = ' ABCDEFGHIJKLMNOPQRSTUW'
 let Cells = {}
 
 let isChoice = false
-
-function giveValue(value, c){
+function giveValue(value, c) {
   let number = ''
-  let n=c
+  let n = c
   let x = Letters.indexOf(value[n])
   n++
-  while(value[n]!=='}' && n<value.length){
+  while (value[n] !== '}' && n < value.length) {
     number += value[n]
-    n++ 
+    n++
   }
   let newValue
-  let ce = Cells[x+':'+number]
-  console.log('x,y',x, number)
-  if(ce.result){newValue=ce.result}else{newValue=ce.value}
-  let skip = n-c
+  let ce = Cells[x + ':' + number]
+  console.log('x,y', x, number)
+  if (ce.result) {
+    newValue = ce.result
+  } else {
+    newValue = ce.value
+  }
+  let skip = n - c
   return [newValue, skip]
 }
-function parse(value){
-    
-    if(value[0]==='='){
-      value = value.toUpperCase()
-      //finding links to other cells
-      let newValue = ''
-      for(c=0;c<value.length;c++){
-        
-        if(value[c]==='{'){
-          c+=1
-          let [rep, ind] = giveValue(value, c)
-          newValue += rep
-          c+=ind
-        }else{
-          newValue += value[c]
-        }
-      }
-      newValue[0]=''
-      console.log(newValue)
-      try{
-        let result = Formula.calc(newValue)
-        console.log(result)
-        return result
-      }catch{
-        return 'Error'
+
+function parse(value) {
+
+  if (value[0] === '=') {
+    value = value.toUpperCase()
+    //finding links to other cells
+    let newValue = ''
+    for (c = 0; c < value.length; c++) {
+
+      if (value[c] === '{') {
+        c += 1
+        let [rep, ind] = giveValue(value, c)
+        newValue += rep
+        c += ind
+      } else {
+        newValue += value[c]
       }
     }
+    newValue[0] = ''
+    console.log(newValue)
+    try {
+      let result = Formula.calc(newValue)
+      console.log(result)
+      return result
+    } catch {
+      return 'Error'
+    }
+  }
 
 }
-class CELL{
-  constructor(x, y){
+class CELL {
+  constructor(x, y) {
     this.x = x
     this.y = y
     //what is typed in cell
     this.value = ''
-    
+
     //what is displayed to cell
     this.result = null
 
@@ -65,102 +69,123 @@ class CELL{
     this.target
   }
 
-  focus(value){
+  focus(value) {
     value.target.value = this.value
     this.target = value.target
   }
-  blur(value){
+  blur(value) {
     this.value = value.target.value
 
     // update every cell
-    for(y=0;y<25;y++){
-      for(x=0;x<20;x++){
-        if(x!==0 && y!==0){
-          Cells[x+':'+y].update()
+    for (y = 0; y < 25; y++) {
+      for (x = 0; x < 20; x++) {
+        if (x !== 0 && y !== 0) {
+          Cells[x + ':' + y].update()
 
         }
       }
     }
   }
-  update(){
+  update() {
     let result = parse(this.value)
-    if(result){
-      if(result === 'Error'){
+    if (result) {
+      if (result === 'Error') {
         this.target.style.color = 'red'
-      }else{
+      } else {
         this.target.style.color = 'black'
         this.result = result
       }
       this.target.value = result
+    } else {
+      this.target.value = this.value
     }
-
   }
 }
 
-function make_cells(y, x){
+function make_cells(y, x) {
   let cell;
-  if(y===0){
-    cell = document.createElement('h3') 
+  if (y === 0) {
+    cell = document.createElement('h3')
     cell.textContent = Letters[x]
-  }else if(x===0){
-    cell = document.createElement('h3') 
+  } else if (x === 0) {
+    cell = document.createElement('h3')
     cell.textContent = y
 
-  }else{
+  } else {
     cell = document.createElement('input')
-    cell.id = x+':'+y
-    cell_class = new CELL(x, y) 
-    Cells[x+':'+y] = cell_class
-
-    document.addEventListener('keypress', (key)=>{
-      if(key.keyCode===13){
-        isChoice = false
-        current.blur()
+    cell.id = x + ':' + y
+    cell_class = new CELL(x, y)
+    cell_class.target = cell
+    if (y === 1) {
+      switch (x) {
+        case 1:
+          cell_class.value = 3
+          break
+        case 2:
+          cell_class.value = 3
+          break
+        case 3:
+          cell_class.value = '={A1}+{B1}'
+          break
       }
-    })
-    
-    cell.addEventListener('click', (value)=>{
-      if(isChoice){
+    }
+
+    cell_class.update()
+    Cells[x + ':' + y] = cell_class
+
+
+    cell.addEventListener('click', (value) => {
+      if (isChoice) {
         value.target.blur()
         current.value += '{' + Letters[x] + y + '}'
         console.log('clicked', value)
       }
+      
     })
-    cell.addEventListener('blur', (value)=>{
-      if(!isChoice){
-        Cells[x+':'+y].blur(value)
-      }
-
-    })
-    
-    cell.addEventListener('input', (value)=>{
-      if(value.target.value[0] === '='){
-        isChoice=true
-      }else{
-        isChoice=false
+    cell.addEventListener('blur', (value) => {
+      if (!isChoice) {
+        Cells[x + ':' + y].blur(value)
       }
     })
-    cell.addEventListener('focus', (value)=>{
-      if(!isChoice){
 
+    cell.addEventListener('input', (value) => {
+      if (value.target.value[0] === '=') {
+        isChoice = true
+        main.classList.add('cell')
+      } else {
+        isChoice = false
+        main.classList.remove('cell')
+      }
+    })
+    cell.addEventListener('focus', (value) => {
+      if (!isChoice) {
         current = value.target
-        Cells[x+':'+y].focus(value)
-      }else{
+        Cells[x + ':' + y].focus(value)
+      } else {
         current.focus()
       }
-
     })
   }
-  const main = document.getElementById('main')
-   
-  cell.className = cellStyles
-  cell.style.top = (2*y).toString()+'rem'
-  cell.style.left = (5*x).toString()+'rem'
-  main.appendChild(cell)
-}
-for(y=0;y<25;y++){
-  for(x=0;x<20;x++){
 
-  make_cells(y, x)
+  const main = document.getElementById('main')
+  cell.className = cellStyles
+  cell.style.top = (2 * y).toString() + 'rem'
+  cell.style.left = (5 * x).toString() + 'rem'
+  main.appendChild(cell)
+
+}
+
+document.addEventListener('keypress', (key) => {
+  if (key.keyCode === 13) {
+    isChoice = false
+    main.classList.remove('cell')
+    current.blur()
+  }
+})
+
+for (y = 0; y < 25; y++) {
+  for (x = 0; x < 20; x++) {
+
+    make_cells(y, x)
   }
 }
